073fe09cbbe60920c3d741fe878f5509d6a2ecf1
mantzuri <idanmantzor@gmail.com>
2021-08-11 08:47:47 -0600

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import _ from 'lodash';
-import webpack from 'webpack';
+// import webpack from 'webpack';
 
 function component() {
   const element = document.createElement('div');
-
-  const handler = (percentage, message, ...args) => {
-    // e.g. Output each progress message directly to the console:
-    console.info(percentage, message, ...args);
-  };
-
-  new webpack.ProgressPlugin(handler);
-
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
